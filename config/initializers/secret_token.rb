33b7f080f594c39c5cf31d5029ffa41bcb568a22
Brandon Cox <napcracka@Brandon-Coxs-MacBook-Pro.local>
2011-06-13 23:35:51 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NKHSReunion::Application.config.secret_token = '46e96807e547e1897471faaf0a1d83d06adac54acb084dea25c09cbaec6eb02019e9c94ffc8c76650b11eef841b042652a3677269921807cf3af7f45c1d2cf91'
